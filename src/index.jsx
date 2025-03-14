01a43816c9cf0cc47d40b58e9ff585784491930b
Riccardo Moncada <rmondev@gmail.com>
2025-03-14 17:27:56 -0400

    Initial Commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,6 @@
+//Title: Scrimba Capstone 1 Tenzies Game
+//Developed by: Riccardo Moncada (rmondev)
+
 import {createRoot} from 'react-dom/client'
 import App from '../src/App'
 const root = createRoot(document.getElementById('root'))
