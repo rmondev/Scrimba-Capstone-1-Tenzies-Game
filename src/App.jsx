fae7ace0849cad734c235dc31b35883a0068f861
Riccardo Moncada <rmondev@gmail.com>
2025-03-16 15:15:13 -0400

    added array of dice objects, hold dice value functionality, "gameWon" condition check, renaming of "roll" button when game is one to start a new game (regenerate dice) and confetti celebration on "gameWon" = true.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,96 @@
-/**
- * Challenge:
- * 
- * - Create a Die component that takes a `value` prop. Should
- *   render a button with that value displayed.
- * - Render 10 instances of the Die component (manually)
- *      - Provide a number between 1-6 for the value on each
- *        for now
- * - Style the <main> and <Die> components 
- *   to look like they do in the slide
- *      - Hints: Create a container to hold the 10 instances
- *        of the Die component, and use CSS Grid to lay them
- *        out evenly in 2 rows of 5 columns
- *      - Use flexbox on main to center the dice container
- *        in the center of the page
- */
-
+import { useState, useEffect} from 'react'
+import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 import Die from '../src/components/Die'
 
 function App() {
 
+const [windowDimension, setWindowDimension] = useState({width: window.innerWidth, height: window.innerHeight})
 
+useEffect(()=>{
+
+  console.log('Effect Triggered!')
+  window.addEventListener("resize", ()=>{
+    setWindowDimension({
+      width: window.innerWidth, 
+      height: window.innerHeight})
+  })
+}, [])
+
+
+  const generateAllNewDice = () => {
+    return new Array(10)
+      .fill(0)
+      .map(()=> ({
+        value: Math.ceil(Math.random()*6), 
+        isHeld: false,
+        id: nanoid()}))
+  }
+
+  const rollDice = () => {
+
+    gameWon ? setDice(generateAllNewDice()) : setDice(prevDice => prevDice.map(item=> { return item.isHeld ? item : {...item, value: Math.ceil(Math.random()*6)}}))
+
+    // if (gameWon) {
+    
+    //   setDice(generateAllNewDice())
+    // } else {
+    //   setDice(prevDice => prevDice.map(item=> {
+    //     return item.isHeld ? item : {...item, value: Math.ceil(Math.random()*6)}
+    // }))
+    // }
+  }
+
+
+  const hold = (id) => {
+    setDice(prevDice => prevDice.map(item=> {
+      return item.id === id ? {...item, isHeld: !item.isHeld} : item
+    }))
+  }
+
+
+
+
+  const [dice, setDice] = useState(() => generateAllNewDice())
+ 
+ 
+
+   // Winning Condition (check every die 'isHeld' value in the array for truthiness, and check if all values of the die are the same)
+  let gameWon = dice.every(die => die.isHeld) && dice.every(die=> die.value === dice[0].value)
+
+
+  //Window Game-One
+
+
+  const diceElements = dice.map(dieObject => (
+    <Die 
+      key={dieObject.id} 
+      id={dieObject.id} 
+      value={dieObject.value} 
+      isHeld={dieObject.isHeld} 
+      // hold={() => hold(dieObject.id)}
+      hold={ ()=>hold(dieObject.id) }
+      
+      />
+    ))
+    
+  
 
   return (
   
     <main>
-      <div className='dice-container'>
-        <Die value={6}/>
-        <Die value={3}/>
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={4}/>
-        <Die value={3}/>
-        <Die value={1}/>
-        <Die value={2}/>
+      {gameWon && <Confetti gravity={0.05} numberOfPieces={300}width={windowDimension.width} height={windowDimension.height}/>}
+      <div aria-live='polite' className='sr-only'>
+        {gameWon && <p>Congratulations! You win! Press "New Game" to play again.</p>}
       </div>
-      
+       <h1 className="title">Tenzies</h1>
+       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className='dice-container'>
+        {diceElements}
+      </div>
+      <button className='roll-dice' onClick={rollDice}>
+        {gameWon ? 'New Game' : 'Roll'}
+        </button>
     </main>
 
   
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Die = (props) => {
+
+
+
   return (
-    <button>
+    <button 
+      style={{backgroundColor: props.isHeld ? '#59E391' : null}}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value of ${props.value}, is ${props.isHeld ? 'held' : 'not held'}`}
+    >
         {props.value}
     </button>
   )
